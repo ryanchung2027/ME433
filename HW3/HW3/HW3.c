7d3fc46e324c9da1a4a1ca5f3afe361f62d66837
ryanchung2027 <ryanchung2027@u.northwestern.edu>
2025-04-11 00:09:39 -0500

    added led functions from blink and the example code from the HW3 Git page

--- HW3/HW3/HW3.c
+++ HW3/HW3/HW3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 // define GPIO pins
 #define GPIO_LED_PIN 19
@@ -31,7 +32,7 @@ int main()
     while (!stdio_usb_connected()) {
         sleep_ms(100);
     }
-    printf("Start!\n");
+    
  
     while (1) {
         char message[100];
@@ -39,4 +40,30 @@ int main()
         printf("message: %s\r\n",message);
         sleep_ms(50);
     }
+}
+
+
+// Perform led initialisation
+int pico_led_init(void) {
+    #if defined(GPIO_LED_PIN)
+        // A device like Pico that uses a GPIO for the LED will define GPIO_LED_PIN
+        // so we can use normal GPIO functionality to turn the led on and off
+        gpio_init(GPIO_LED_PIN);
+        gpio_set_dir(GPIO_LED_PIN, GPIO_OUT);
+        return PICO_OK;
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // For Pico W devices we need to initialise the driver etc
+        return cyw43_arch_init();
+    #endif
+    }
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+#if defined(GPIO_LED_PIN)
+    // Just set the GPIO on or off
+    gpio_put(GPIO_LED_PIN, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Ask the wifi "driver" to set the GPIO on or off
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
 }
