bc305e0344f65cdc72327269c6855965ec42ef5c
ryanchung2027 <ryanchung2027@u.northwestern.edu>
2025-04-15 12:24:36 -0500

    started HW4

---
+++ HW4/HW4_code/HW4_code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+static inline void cs_select(uint cs_pin);
+static inline void cs_deselect(uint cs_pin);
+
+int main()
+{
+    stdio_init_all();
+
+    spi_init(spi_default, 1000 * 1000); // the baud, or bits per second
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+}
+
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
