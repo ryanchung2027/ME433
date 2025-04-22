3ca8509a9a47dfefa3f4c7e9ca283984cf7709d6
ryanchung2027 <ryanchung2027@u.northwestern.edu>
2025-04-22 12:24:15 -0500

    added the same code for addition to subtraction, multiplication and division

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -33,21 +33,49 @@ int main()
     printf("Enter two floats to use:");
     scanf("%f %f", &f1, &f2);
     volatile float f_add, f_sub, f_mult, f_div;
+
     // run addition 1000 times
     absolute_time_t t1_init = get_absolute_time();
-    uint64_t t_init = to_us_since_boot(t1_init);
     for (i = 0; i < 1000; i++) {
         f_add = f1+f2;
     }
     absolute_time_t t1_add = get_absolute_time();                   // get time for all addition operations
-    uint64_t t_add = to_us_since_boot(t1_add-t1_init);
-    int cycles_add = t_add / (6.667 / 1000 * 1000);                        // convert to cycles for one operation by dividing by 6.667 ns and 1000 cycles
-    printf("\ncycles for addition = %d\n", cycles_add);
-    printf("t_init = %llu, t_add = %llu", t_init, t_add);
-    // f_sub = f1-f2;
-    // f_mult = f1*f2;
-    // f_div = f1/f2;
-    // printf("\nResults: \n%f+%f=%f \n%f-%f=%f \n%f*%f=%f \n%f/%f=%f\n", f1,f2,f_add, f1,f2,f_sub, f1,f2,f_mult, f1,f2,f_div);
+    
+    // run subtraction 1000 times
+    for (i = 0; i < 1000; i++) {
+        f_sub = f1-f2;
+    }
+    absolute_time_t t1_sub = get_absolute_time();                   // get time for all subtraction operations
+
+    // run multiplcation 1000 times
+    for (i = 0; i < 1000; i++) {
+        f_mult = f1*f2;
+    }
+    absolute_time_t t1_mult = get_absolute_time();                   // get time for all multiplication operations
+    
+    // run divison 1000 times
+    for (i = 0; i < 1000; i++) {
+        f_div = f1/f2;
+    }
+    absolute_time_t t1_div = get_absolute_time();                   // get time for all division operations
+
+    // find time it takes for each operation to work
+    uint64_t t_init = to_us_since_boot(t1_init);                 // convert initial time to ms
+    uint64_t t_add = to_us_since_boot(t1_add);
+    int cycles_add = (t_add - t_init) / (6.667 / 1000 * 1000);   // convert to cycles for one operation by dividing by 6.667 ns and 1000 cycles
+    uint64_t t_sub = to_us_since_boot(t1_sub);
+    int cycles_sub = (t_sub - t_add) / (6.667 / 1000 * 1000);    // convert to cycles for one operation by dividing by 6.667 ns and 1000 cycles
+    uint64_t t_mult = to_us_since_boot(t1_mult);
+    int cycles_mult = (t_mult - t_sub) / (6.667 / 1000 * 1000);  // convert to cycles for one operation by dividing by 6.667 ns and 1000 cycles
+    uint64_t t_div = to_us_since_boot(t1_div);
+    int cycles_div = (t_div - t_mult) / (6.667 / 1000 * 1000);   // convert to cycles for one operation by dividing by 6.667 ns and 1000 cycles    
+    
+
+    printf("\nResults: \n%f+%f=%f \n%f-%f=%f \n%f*%f=%f \n%f/%f=%f\n", f1,f2,f_add, f1,f2,f_sub, f1,f2,f_mult, f1,f2,f_div);
+    printf("\nCycles for addition = %d", cycles_add);
+    printf("\nCycles for subtraction = %d", cycles_sub);
+    printf("\nCycles for multiplication = %d", cycles_mult);
+    printf("\nCycles for division = %d", cycles_div);
 
     
     
