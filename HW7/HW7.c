267ac526b316330ebf70dce6320d9759506dd617
ryanchung2027 <ryanchung2027@u.northwestern.edu>
2025-06-03 04:03:19 -0500

    started hw7

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -10,6 +10,8 @@
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
+#define HEARTBEAT_INTERVAL 500
+#define PICO_DEFAULT_LED_PIN 16
 
 
 
@@ -17,19 +19,26 @@ int main()
 {
     stdio_init_all();
 
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
-    
+
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
 
     // initialize screen
     ssd1306_setup();
 
     while (true) {
-        printf("Hello, world!\n");
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        ssd1306_drawPixel(0, 0, 1);
+        ssd1306_update();
+        sleep_ms(1000);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        ssd1306_clear();
         sleep_ms(1000);
     }
 }
