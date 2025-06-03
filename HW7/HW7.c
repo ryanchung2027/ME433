f03f67303d5f692762e5d43557603324f1901f07
ryanchung2027 <ryanchung2027@u.northwestern.edu>
2025-06-03 03:29:00 -0500

    included ssd1306 and font header files into hw7 file + started code

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "ssd1306.h"
+#include "font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -22,7 +24,9 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    // initialize screen
+    ssd1306_setup();
 
     while (true) {
         printf("Hello, world!\n");
